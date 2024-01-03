1777b9e2676bcdb5b5a79658a8d5654d008ed24b
Gsubhashreddy <gsubhashreddy1@gmail.com>
2024-01-02 19:55:15 -0800

    Time: 265 ms (10.76%), Space: 19.5 MB (6.86%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,24 +1,21 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        rows=len(bank)
-        col=len(bank[0])
         res=0
         row1=0
+        col=len(bank[0])
         # Iterate through each row
-        for i in range(rows):
-            
+        for i in range(col):
+            if bank[0][i]=="1":
+                row1+=1
+        for i in range(1,len(bank)):            
             # Go over each col and get no.of 1's
             count1=0
             for j in range(col):
                 if bank[i][j]=="1":
                     count1+=1   # Gives total no of 1's in row i
-            if i==0:
-                # Initail row, So No beams above it
+            if count1!=0:
+                res+=(row1*count1)
                 row1=count1
-            else:
-                if count1!=0:
-                    res+=(row1*count1)
-                    row1=count1
         return res
                 
             
