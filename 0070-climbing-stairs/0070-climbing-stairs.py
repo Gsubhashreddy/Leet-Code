4687e46813f022fdf836fd986cf77ffcb0a02648
Gsubhashreddy <gsubhashreddy1@gmail.com>
2024-01-17 19:26:31 -0800

    Time: 40 ms (39.37%), Space: 17.3 MB (15.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    # def helper(self,n,s):
+    #     if s==n:
+    #         self.res+=1
+    #         return
+    #     if s>n:
+    #         return
+    #     self.helper(n,s+1)
+    #     self.helper(n,s+2)
+        
+        
+    def climbStairs(self, n: int) -> int:
+        res=[1,2]
+        if n ==1:
+            return 1
+        elif n==2:
+            return 2
+        
+        for i in range(2,n):
+            res.append(res[i-1]+res[i-2])
+            # res[i-1] = max(res[i-2],0)
+        return res[-1]
