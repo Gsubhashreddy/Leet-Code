fd9636b5ba61069d22c9374831b52dc24b87d28a
Gsubhashreddy <gsubhashreddy1@gmail.com>
2024-04-26 23:56:23 -0700

    Time: 1236 ms (11.30%), Space: 184.3 MB (17.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,30 +1,16 @@
 class Solution:
-    
-    # def helper(self, t1, t2, i1, i2,res):
-    #     # Base
-    #     if i1>= len(t1) or i2>=len(t2):
-    #         return res
-    #     # Logic
-    #     if t1[i1] == t2[i2]:
-    #         c1 = self.helper(t1,t2,i1+1,i2+1, res+1)
-    #         return c1
-    #     else:
-    #         c1 = self.helper(t1,t2,i1+1,i2, res)
-    #         c2 = self.helper(t1,t2,i1,i2+1, res)
-    #         return max(c1,c2)
-    
+    ma = 0
+    def d(self, i , j, t1,t2, m,n,co):
+        if i>=m or j >=n:
+            return 0
+        if (i,j) not in self.di:
+            if t1[i] == t2[j]:
+                self.di[(i,j)]  = 1 + self.d(i+1,j+1,t1,t2,m,n,co)
+                # self.ma = max(self.ma, co)
+            else:
+                self.di[(i,j)] = max(self.d(i+1,j,t1,t2,m,n,co), self.d(i,j+1,t1,t2,m,n,co))
+        return self.di[(i,j)]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0 for i in range(len(text1)+1)] for j in range(len(text2)+1)]
-        # print(dp)
-        for i in range(len(text2)):
-            for j in range(len(text1)):
-                k,l = i+1, j+1
-                # print(k,l)
-                if text2[i]==text1[j]:
-                    
-                    dp[k][l] = 1+ dp[k-1][l-1]
-                else:
-                    dp[k][l] = max(dp[k-1][l],dp[k][l-1])
-        
-        return dp[-1][-1]
+        self.di = {}
+        return self.d(0,0,text1, text2,len(text1),len(text2), 0)
         
